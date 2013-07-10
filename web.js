dd5e7b697bcd9ee55e564616e07e087f553bded0
tseto <tseto@tseto-VirtualBox.(none)>
2013-07-10 11:38:50 -0700

    updated

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = new Buffer(256);
-    buf.write(fs.readFileSync("index.html"));
-    response.send(buf.toString());
+//    var buf = new Buffer(256);
+//    buf.write(fs.readFileSync("./index.html"));
+//    response.send(buf.toString());
+    response.send(fs.readFileSync("./index.html"));
 });
 
 var port = process.env.PORT || 5000;
