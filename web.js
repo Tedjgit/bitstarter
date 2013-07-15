34dc817e7f836528237ea822201e3224b3f0bb53
Tedjgit <Tedj7890@gmail.com>
2013-07-15 15:00:53 -0700

    added hw3, hw4, 1

--- web.js
+++ web.js
@@ -8,6 +8,17 @@ app.get('/', function(request, response) {
     response.send(buf.toString());
 });
 
+app.get('/hw3', function(request, response) {
+    var buf = fs.readFileSync("bitstarter-hw3.html");
+    response.send(buf.toString());
+});
+
+app.get('/hw4', function(request, response) {
+    var buf = fs.readFileSync("bitstarter-hw4.html");
+    response.send(buf.toString());
+});
+
+
 app.get('/1', function(request, response) {
     response.send("1");
 });
